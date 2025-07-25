558f35d018e97816fa7537b79fabfed866ed1e73
Victor Manuel Gomez <victormanuel272791@egmail.com>
2025-07-24 21:20:59 -0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/SIVIGILA-portal-frontend',
+  base: '/SIVIGILA-portal-frontend/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
