b6e268bf10393f630a0d9ce6243de9c891bb5586
Victor Manuel Gomez <victormanuel272791@egmail.com>
2025-07-25 18:27:23 -0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,6 +9,11 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: ['sivigila-portal-frontend-1081794814967.europe-west1.run.app'],
   },
   resolve: {
     alias: {
